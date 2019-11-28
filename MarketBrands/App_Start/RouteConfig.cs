c0bd59dd4274caadae0e6af2f162225bdbb6a4dc
Mustafa Muhammad <13590052+Moskaoud@users.noreply.github.com>
2019-11-28 13:56:34 +0200

    ASP.net MVC

--- MarketBrands/App_Start/RouteConfig.cs
+++ MarketBrands/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MarketBrands
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Markets", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
